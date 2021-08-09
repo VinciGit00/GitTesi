1582e050cbd49bf47c44cdefb2c8a7d9c2a3d610
MarcoVinciguerra <mvincig11@gmail.com>
2021-08-09 18:24:20 +0200

    compitoNH3

---
+++ DownloadData/Compito/NH3PM.R
@@ -0,0 +1,17 @@
+library(ARPALData)
+library(tidyverse)
+
+datipoll = get_ARPA_Lombardia_AQ_data(
+  ID_station = NULL,
+  Year = c(2019), #singolo anno: numero, oppure parentesi per costruire un vettore di anni
+  Frequency = "daily", #Frequenza: oraria
+  Var_vec = NULL,
+  Fns_vec = NULL,
+  by_sensor = 0,
+  verbose = T
+)
+
+datashort = datipoll[, c(1:4, 12:13)]
+
+setwd('/Users/marcovinciguerra/Github/GitTesi/DownloadData/Compitp') 
+write_csv(datashort, "datipoll.csv")
