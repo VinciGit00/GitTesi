bd9c7dcdcd7f4d3ebe9f38bfc331e8ebc55c1d0c
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2021-09-03 20:59:23 +0200

    reorder code

--- DownloadData/MissingDataPollution.R
+++ DownloadData/MissingDataPollution.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(ggfortify)
 library(lubridate)
 
-source("/Users/marcovinciguerra/Github/GitTesi/DownloadData/Functions.R", encoding = 'UTF-8')
+source("~/GitHub/BachelorThesis/Rstudio code/Functions.R", encoding = 'UTF-8')
 
 #Filter datas
 registry <- get_ARPA_Lombardia_AQ_registry()
@@ -27,6 +27,8 @@ bestcentralines <- RegistryRed %>%
   filter(n>=2) %>% 
   distinct(IDStation) %>%
   pull() # stations that measure at least 2 of the variables at the same time
+
+
 
 #Starting with the loop for downloading the data + casting of the data
 
@@ -92,8 +94,8 @@ for(index in startyear:lastyear) {
                                   on ma.IDStation = mtodos.IDStation
                                 ')
   name = paste("Missing",index,".csv",sep="" )
-  setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData/MissingTables")
-  write_csv(tableMissingDatasTotal[[index-startyear+1]], name)
+  #setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData/MissingTables")
+  #write_csv(tableMissingDatasTotal[[index-startyear+1]], name)
 }
 
 #Creating the table of yes/no
@@ -139,10 +141,29 @@ presencetable <- sqldf("SELECT c25.IDStation, C25.NameStation, c25.PM25, c10.PM1
                  ON c25.IDStation = c10.IDStation
                  JOIN ColumnA ca
                  ON c25.IDStation = ca.IDStation")
-setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData")
-write.csv(presencetable, "presencetable.csv")
+#setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData")
+#write.csv(presencetable, "presencetable.csv")
 
-#Data plot of the centralines with all the sensors
+#Plot of the best centralines
+
+presencetable <- presencetable %>%
+  mutate(Etichetta = case_when(PM10 == 1 & PM25 == 1 & Ammonia == 1 ~ "Tutti",
+                               PM10 == 1 & PM25 == 1 & Ammonia == 0 ~ "PM10-PM2.5",
+                               PM10 == 1 & PM25 == 0 & Ammonia == 1 ~ "PM10-NH3",
+                               PM10 == 0 & PM25 == 1 & Ammonia == 1 ~ "PM2.5-NH3",
+                               PM10 == 1 & PM25 == 0 & Ammonia == 0 ~ "PM10",
+                               PM10 == 0 & PM25 == 1 & Ammonia == 0 ~ "PM2.5",
+                               PM10 == 0 & PM25 == 0 & Ammonia == 1 ~ "NH3"))
+
+presencetable_red <- presencetable %>%
+  select(IDStation,Etichetta)
+
+RegistryRed <- full_join(RegistryRed,presencetable_red,by = c("IDStation"))
+
+map_Lombardia_stations_custom(RegistryRed,col_points = Etichetta)
+
+#win.graph()
+map_Lombardia_stations_custom(RegistryRed)
 
 
 presencetable <- NULL
@@ -263,24 +284,4 @@ for (i in 1:length(lastYearStations)) {
 }
 
 BlueStripes(FullStations,"2018-2020")
-
-#Lombardy map
-#Plot of the best centralines
-map_Lombardia_stations_custom(RegistryRed)
-
-presencetable <- presencetable %>%
-  mutate(Etichetta = case_when(PM10 == 1 & PM25 == 1 & Ammonia == 1 ~ "Tutti",
-                               PM10 == 1 & PM25 == 1 & Ammonia == 0 ~ "PM10-PM2.5",
-                               PM10 == 1 & PM25 == 0 & Ammonia == 1 ~ "PM10-NH3",
-                               PM10 == 0 & PM25 == 1 & Ammonia == 1 ~ "PM2.5-NH3",
-                               PM10 == 1 & PM25 == 0 & Ammonia == 0 ~ "PM10",
-                               PM10 == 0 & PM25 == 1 & Ammonia == 0 ~ "PM2.5",
-                               PM10 == 0 & PM25 == 0 & Ammonia == 1 ~ "NH3"))
-
-presencetable_red <- presencetable %>%
-  select(IDStation,Etichetta)
-
-RegistryRed <- full_join(RegistryRed,presencetable_red,by = c("IDStation"))
-
-map_Lombardia_stations_custom(RegistryRed,col_points = Etichetta)
 
