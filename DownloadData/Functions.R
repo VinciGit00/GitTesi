b5e4d5743c8c8442d94c57e478f0f9afe0bad031
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2021-09-04 15:07:30 +0200

    add 'download' to functions file

--- DownloadData/Functions.R
+++ DownloadData/Functions.R
@@ -146,3 +146,39 @@ map_Lombardia_stations_custom <- function (data, title = "Map of ARPA stations i
     ggplot2::labs(title = title)
   print(geo_plot)
 }
+
+Download <- function(startyear, lastyear, centraline) {
+  #Libraries
+  library(ARPALData)
+  library(tidyverse)
+  library(sqldf)
+  library(ggplot2)
+  library(ggfortify)
+  library(lubridate)
+  
+  data <- NULL
+  cast <- NULL
+  
+  for(index in startyear:lastyear) {
+    #Downloading
+    data[[1+index-startyear]] <- get_ARPA_Lombardia_AQ_data(
+      ID_station = c(centraline),
+      Year = index,
+      Frequency = "daily",
+      Var_vec = NULL,
+      Fns_vec = NULL,
+      by_sensor = 0,
+      verbose = T
+    )
+    #Casting
+    cast[[1+index-startyear]] <- data.frame(data[[1+index-startyear]] )
+    
+    #Renaming
+    cast[[1+index-startyear]] <- cast[[1+index-startyear]]  %>%
+      rename(
+        PM25 = PM2.5
+      )
+  }
+  
+  return(cast)
+}
