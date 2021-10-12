b8c94fd905b7b7972b3339f456977c2c6eedb417
MarcoVinciguerra <mvincig11@gmail.com>
2021-10-12 10:54:01 +0200

    boh

---
+++ DownloadData/Scripts/InterestedCentralines.R
@@ -0,0 +1,11 @@
+#Libraries
+library(ARPALData)
+library(tidyverse)
+library(sqldf)
+library(ggplot2)
+library(ggfortify)
+library(lubridate)
+library(sf)
+
+#Source functions
+source("~/GitHub/GitTesi/DownloadData/Scripts/Functions.R", encoding = 'UTF-8')
